2e87bd60caa063e86e8fd69edea1bb69dc42c5c9
a111222018 <a111222018@mail.shu.edu.tw>
2024-03-22 14:57:24 +0800

    112-03-22

---
+++ UI_TEST/app/src/main/java/com/example/ui_test/DisplayStringActivity.java
@@ -0,0 +1,20 @@
+package com.example.ui_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayStringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_display_string);
+
+        TextView textshow=(TextView) findViewById(R.id.textViewShow);
+        Intent intent=getIntent();
+        textshow.setText(intent.getStringExtra("userInput")+" 您好!");
+    }
+}
--- UI_TEST_2/app/src/main/java/com/example/ui_test_2/MainActivity.java
+++ UI_TEST_2/app/src/main/java/com/example/ui_test_2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.ui_test;
+package com.example.ui_test_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.example.ui_test_2.DisplayStringActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         EditText EditTextinput=(EditText) findViewById(R.id.editTextInput);
         String input=EditTextinput.getText().toString();
 
-        Intent intent=new Intent(this,DisplayStringActivity.class);
+        Intent intent=new Intent(this, DisplayStringActivity.class);
         intent.putExtra("userInput",input);
         startActivity(intent);
     }
