2f9076bf0457d1d9d8cc9e6dfafae7e969b663d2
a111222018 <a111222018@mail.shu.edu.tw>
2024-04-12 15:02:28 +0800

    2024/4/12

--- Ch5_4/app/src/main/java/com/example/ch5_4/MainActivity.java
+++ Ch5_4/app/src/main/java/com/example/ch5_4/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ch5_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public  boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            EditText txtInput=(EditText) findViewById(R.id.txtInput);
+            txtInput.setText("按下返回鍵");
+        }
+        return  true;
+    }
+    @Override
+    public  boolean onKeyUp(int keyCode, KeyEvent event){
+
+        TextView lbl=(TextView) findViewById(R.id.lblOutput);
+        lbl.setText("返回按鍵碼:"+keyCode);
+        return  true;
     }
 }
