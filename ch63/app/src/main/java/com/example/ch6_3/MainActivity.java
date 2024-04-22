295e7dba8f8d8396b8e34e84b16d74df62a34a67
a111222018 <a111222018@mail.shu.edu.tw>
2024-04-22 17:51:56 +0800

    2024/04/22

--- ch63/app/src/main/java/com/example/ch6_3/MainActivity.java
+++ ch63/app/src/main/java/com/example/ch6_3/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.ch6_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaDrm;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity
     private TextView output;
     private EditText txt;
     private RadioGroup rg;
+    private RadioGroup rt;
+
+    private  String str = "";
+    private  String gender="";
+    private  String ticket="";
+    private  int sum=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +31,9 @@ public class MainActivity extends AppCompatActivity
         output = (TextView) findViewById(R.id.lblOutput);
         // 註冊傾聽者物件
         rg = (RadioGroup) findViewById(R.id.rgGender);
+        rt = (RadioGroup) findViewById(R.id.rgticket);
         rg.setOnCheckedChangeListener(this);
+        rt.setOnCheckedChangeListener(this);
         txt = (EditText) findViewById(R.id.txtName);
         txt.addTextChangedListener(this);
     }
@@ -32,28 +42,37 @@ public class MainActivity extends AppCompatActivity
     }
     @Override
     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        show(rg.getCheckedRadioButtonId());
+        show(rg.getCheckedRadioButtonId(),"txt");
     }
     @Override
     public void afterTextChanged(Editable editable) {
     }
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int i) {
-        show(i);
-    }
-    public void show(int i) {
-        String str = "";
-        str = txt.getText().toString() + "\n";  // 取得EditText值
-        // 判斷是選擇哪一個, 使用的參數i
-        switch (i) {
-            case R.id.rdbBoy:
-                RadioButton boy = (RadioButton) findViewById(R.id.rdbBoy);
-                output.setText(str + boy.getText());
-                break;
-            case R.id.rdbGirl:
-                RadioButton girl = (RadioButton) findViewById(R.id.rdbGirl);
-                output.setText(str + girl.getText());
-                break;
+
+        if(radioGroup==rg){
+            show(i,"rg");
         }
+        else{
+            show(i,"rt");
+        }
+        //Log.v("test", String.valueOf(i)); // Verbose (最詳細) 日誌
+        //show(i);
+    }
+    public void show(int i,String g) {
+
+        str = txt.getText().toString() + "張";
+        RadioButton selcted = (RadioButton) findViewById(i);
+        if(g=="rg"){
+            gender= (String) selcted.getText();
+        }
+        else if(g=="rt"){
+            ticket= (String) selcted.getText();
+
+        }
+
+        String[] ticketype = ticket.split("\n");
+        sum=Integer.parseInt(ticketype[0])*Integer.parseInt(ticketype[1]);
+        output.setText(gender+"\n"+ticketype[0]+str+"\n"+"金額"+sum );
     }
 }
