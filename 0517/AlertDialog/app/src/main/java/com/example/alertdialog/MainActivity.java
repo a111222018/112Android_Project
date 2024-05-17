c2ce13af392df6e9e70af6c30f51ccaeb10484aa
a111222018 <a111222018@mail.shu.edu.tw>
2024-05-17 15:03:07 +0800

    2024/05/17

--- 0517/AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.java
+++ 0517/AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.alertdialog;
 
+import static java.lang.Integer.valueOf;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
             new AlertDialog.Builder(MainActivity.this)
                     .setTitle("選擇顏色")
                     .setCancelable(true)
-                    .setItems(new String[]{"紅色", "綠色", "藍色","粉色"}, (dialog, which) -> {
+                    .setItems(new String[]{"紅色", "綠色", "藍色","粉色","螢光綠"}, (dialog, which) -> {
                         switch (which) {
                             case 0:
                                btncolor.setBackgroundColor(0xFFFF0000);
@@ -54,9 +59,43 @@ public class MainActivity extends AppCompatActivity {
                             case 3:
                                 btncolor.setBackgroundColor(0xFFFF00FF);
                                 break;
+                            case 4:
+                                btncolor.setBackgroundColor(0xFF00FF00);
+                                break;
                         }
                     })
+                    .setNegativeButton("取消", (dialog, which) -> Toast.makeText(MainActivity.this, "取消變更顏色", Toast.LENGTH_LONG).show())
                     .show();
         });
+
+        Button btnselect = findViewById(R.id.btnselect);
+        ArrayList<String> selected = new ArrayList<String>();
+        String[] type=new String[]{"samsun", "oppo", "nokia", "apple"};
+        TextView output= findViewById(R.id.txtoutput);
+        output.setText("");
+        btnselect.setOnClickListener(v -> {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("選擇項目")
+                    .setCancelable(true)
+                    .setMultiChoiceItems(new String[]{"samsun", "oppo", "nokia", "apple"}, new boolean[]{false, false, false, false, false}, (dialog, which, isChecked) -> {
+                        if (isChecked) {
+                            selected.add(type[valueOf(which)]);
+                            //Toast.makeText(MainActivity.this, "選擇" + which, Toast.LENGTH_LONG).show();
+                        } else {
+                            selected.remove(String.valueOf(which));
+                            //Toast.makeText(MainActivity.this, "取消選擇" + which, Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    //同時放兩個動作
+                    .setPositiveButton("確定", (dialog, which) -> {
+                        output.setText("");
+                        output.setText(selected.toString());
+                        selected.removeAll(selected);
+                    })
+                    .setNegativeButton("取消", (dialog, which) -> Toast.makeText(MainActivity.this, "取消選擇", Toast.LENGTH_LONG).show())
+                    .show();
+        });
+
+
     }
 }
